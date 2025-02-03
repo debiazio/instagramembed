7923788a040382397ba7704537533cbba64e40a5
Bruno de Biazio <biazzioxbox@gmail.com>
2025-02-03 14:19:30 -0300

    fgirst

---
+++ react/instagramembed.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const instagramembed: StorefrontFunctionComponent = () => {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+    </div>
+  )
+}
+export default instagramembed
